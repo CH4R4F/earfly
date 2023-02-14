78b320691dae4d935fb3ec2872aa633b0f16f1d4
charaf marghin <developermarghin@gmail.com>
2023-02-14 16:48:15 +0100

    add initial setup

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
