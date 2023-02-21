79db9e134f00e21f9e91efa51e958068431e0903
charaf marghin <developermarghin@gmail.com>
2023-02-21 14:03:54 +0100

    add splash screen with java configuration for android

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StatusBar, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import Intro from './src/navigation/Intro';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <Text>Hello world</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      {/* <StatusBar hidden={true} /> */}
+      <Intro />
+    </NavigationContainer>
   );
 }
 
--- android/app/src/main/java/com/earfly/MainActivity.java
+++ android/app/src/main/java/com/earfly/MainActivity.java
@@ -1,6 +1,8 @@
 package com.earfly;
 
+import android.os.Bundle; // here
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // here
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
@@ -14,6 +16,12 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "earfly";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this);  // here
+    super.onCreate(savedInstanceState);
   }
 
   /**
